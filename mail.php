0fb7428f085e81d047c2e9c1351bf312c597ba49
michaeldaws <108394705+michaeldaws@users.noreply.github.com>
2022-08-30 17:20:22 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,13 +1,13 @@
 <?php
 
-$name - $_POST['name'];
-$email - $_POST['email'];
-$subject - $_POST['subject'];
-$message - $_POST['message'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-$mailheader - "From: ".$name."<".$email.">\r\n";
+$mailheader = "From: ".$name."<".$email.">\r\n";
 
-$recipient - "micdaw@hotmail.com";
+$recipient = "micdaw@hotmail.com";
 
 mail($recipient, $subject, $message, $mailheader);
 or die("Error!");
